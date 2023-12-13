717de3387b31846289c91ff76e01ccf250548e4c
berkaysson <berkaysonel85@gmail.com>
2023-12-13 17:37:18 +0300

    Create UserIdentity inherited from IdentityUser and add relation with UserProfile

---
+++ harmonii.Server/Models/Identity/UserIdentity.cs
@@ -0,0 +1,11 @@
+﻿using harmonii.Server.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace harmonii.Server.Models.Identity
+{
+    public class UserIdentity : IdentityUser<int>
+    {
+        public int UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
+    }
+}
