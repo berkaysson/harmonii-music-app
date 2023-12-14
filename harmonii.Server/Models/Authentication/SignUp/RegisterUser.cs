d263251145407ef89849cea281298c34a5437c92
berkaysson <berkaysonel85@gmail.com>
2023-12-14 16:22:39 +0300

    Create a register modal for form

---
+++ harmonii.Server/Models/Authentication/SignUp/RegisterUser.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace harmonii.Server.Models.Authentication.SignUp
+{
+    public class RegisterUser
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string? UserName {  get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string? Password { get; set; }
+    }
+}
