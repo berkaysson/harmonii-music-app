6e184837fec94cc665425f9917ae909fd2085884
berkaysson <berkaysonel85@gmail.com>
2023-12-14 11:42:55 +0300

    Add collections of songs and playlists

--- harmonii.Server/Models/Entities/UserProfile.cs
+++ harmonii.Server/Models/Entities/UserProfile.cs
@@ -8,5 +8,8 @@ namespace harmonii.Server.Models.Entities
         public string? UserName { get; set; }
         public string? UserImageUrl { get; set; }
         public UserIdentity UserIdentity { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+        public ICollection<Playlist> Playlists { get; set; }
     }
 }
