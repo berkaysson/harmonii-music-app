81ac7ba82d908303f4c30d9612500028be772a47
berkaysson <berkaysonel85@gmail.com>
2023-12-14 16:25:30 +0300

    Create a controller for authentication and add Register post method

---
+++ harmonii.Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,56 @@
+﻿using harmonii.Server.Models;
+using harmonii.Server.Models.Authentication.SignUp;
+using harmonii.Server.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace harmonii.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<UserIdentity> _userManager;
+        private readonly RoleManager<RoleIdentity> _roleManager;
+        private readonly IConfiguration _config;
+        public AuthenticationController(UserManager<UserIdentity> userManager,
+            RoleManager<RoleIdentity> roleManager, 
+            IConfiguration config)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+        {
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden,
+                    new Response { Status = "Error", StatusMessage = "User Already Exist" });
+            }
+
+            UserIdentity user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status201Created, 
+                    new Response { Status = "Success", StatusMessage = "User Created" });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response { Status = "Error", StatusMessage = "User Couldn't Created" });
+            }
+        }
+    }
+}
