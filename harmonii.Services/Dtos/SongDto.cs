dbb92d9f9e5a1bad13c18a1bfdc1c6c380084490
berkaysson <berkaysonel85@gmail.com>
2023-12-18 13:37:18 +0300

    Create SongDto

---
+++ harmonii.Services/Dtos/SongDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonii.Services.Dtos
+{
+    public class SongDto
+    {
+        [Required(ErrorMessage = "Song name is required")]
+        public string SongName { get; set; }
+
+        [Required(ErrorMessage = "Artist name is required")]
+        public string Artist { get; set; }
+
+        //[Url(ErrorMessage = "Cover image URL is not valid")]
+        public string CoverImageUrl { get; set; }
+
+        //[Url(ErrorMessage = "Audio file URL is not valid")]
+        public string AudioFileUrl { get; set; }
+
+        [Required(ErrorMessage = "Genre name is required")]
+        public string GenreName { get; set; }
+    }
+
+}
