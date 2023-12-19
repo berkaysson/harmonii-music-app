368600dd9ef2dad3e32eef8e745df1c6b0a09b5e
berkaysson <berkaysonel85@gmail.com>
2023-12-19 13:56:14 +0300

    CreatePlaylistDetailsDto to send a structured song information to client

---
+++ harmonii.Services/Dtos/PlaylistDetailsDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonii.Services.Dtos
+{
+    public class PlaylistDetailsDto
+    {
+        [Required(ErrorMessage = "Playlist ID is required")]
+        public int PlaylistId { get; set; }
+
+        [Required(ErrorMessage = "Playlist name is required")]
+        public string PlaylistName { get; set; }
+
+        public string PlaylistDescription { get; set; }
+
+        [Required(ErrorMessage = "User profile ID is required")]
+        public int UserProfileId { get; set; }
+
+        [Required(ErrorMessage = "User name is required")]
+        public string UserName { get; set; }
+
+        // Can store only ids of songs
+        public List<SongDetailsDto> Songs { get; set; }
+    }
+}
