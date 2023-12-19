b65e459b09c09eee2f602c9b01c83e09e3f06145
berkaysson <berkaysonel85@gmail.com>
2023-12-19 11:30:44 +0300

    Create PlaylistDto

---
+++ harmonii.Services/Dtos/PlaylistDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonii.Services.Dtos
+{
+    public class PlaylistDto
+    {
+        [Required(ErrorMessage = "Playlist name is required")]
+        public string PlaylistName { get; set; }
+
+        public string PlaylistDescription { get; set; }
+    }
+}
