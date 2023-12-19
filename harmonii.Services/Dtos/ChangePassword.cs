4dabbe5e53612651ecf6d8fb2f548ad6b9bd41cf
berkaysson <berkaysonel85@gmail.com>
2023-12-19 14:57:09 +0300

    Create ChangePassword Dto

---
+++ harmonii.Services/Dtos/ChangePassword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonii.Services.Dtos
+{
+    public class ChangePassword
+    {
+        [Required(ErrorMessage = "Old password is required")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string NewPassword { get; set; }
+    }
+}
