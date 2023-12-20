68912f800f3015129500ef963065ea3f4f05d7c4
berkaysson <berkaysonel85@gmail.com>
2023-12-20 18:48:47 +0300

    Create Dto to send decent Genre information

---
+++ harmonii.Services/Dtos/GenreDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonii.Services.Dtos
+{
+    public class GenreDetailsDto
+    {
+        [Required(ErrorMessage = "Genre id is required")]
+        public int GenreId { get; set; }
+
+        [Required(ErrorMessage = "Genre name is required")]
+        public string GenreName { get; set;}
+        public List<SongDetailsDto> Songs { get; set; }
+    }
+}
