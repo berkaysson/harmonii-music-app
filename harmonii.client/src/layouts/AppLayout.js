a7c44f391e8755d6750798dc62505deca5464ae6
berkaysson <berkaysonel85@gmail.com>
2023-12-22 14:34:04 +0300

    Create AppLayout

---
+++ harmonii.client/src/layouts/AppLayout.js
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
+import RouterComponent from './routers/Router';
+
+const AppLayout = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      {//ScrollToTop component
+      // navigation, footer in browser router
+      }
+        <RouterComponent />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default AppLayout;
