52eade05b0a2cd7ef43193ea2f755ee61aab936c
berkaysson <berkaysonel85@gmail.com>
2023-12-22 14:34:17 +0300

    Implement AppLayout

--- harmonii.client/src/App.jsx
+++ harmonii.client/src/App.jsx
@@ -1,7 +1,12 @@
+import AppLayout from "./layouts/AppLayout";
+
 const App = () => {
 
+
   return (
+    // provide a UserContextProvider as parent of browser router
     <div>
+      <AppLayout />
     </div>
   );
 };
