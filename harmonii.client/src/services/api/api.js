1887e16f7bf0635e33ec1968047d63e0f024c775
berkaysson <berkaysonel85@gmail.com>
2023-12-23 22:08:43 +0300

    Refactor axios instance and fix to wrong token push

--- harmonii.client/src/services/api/api.js
+++ harmonii.client/src/services/api/api.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://localhost:7291/"
+  baseURL: "https://localhost:7291/api"
 });
-
-const token = localStorage.getItem('token');
-if (token) {
-  instance.defaults.headers.common['Authorization'] 
-  = `Bearer ${token}`;
-}
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('user')?.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
