cc2fb31cda19bcc37417dc1928b9f814e715ca08
berkaysson <berkaysonel85@gmail.com>
2023-12-23 12:57:50 +0300

    Create user context provider to handle auth easily

---
+++ harmonii.client/src/services/hooks/useUser.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, createContext } from "react";
+import checkTokenValid from "../auth/checkTokenValid";
+
+const UserContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+export const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [userValid, setUserValid] = useState(false);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser && checkTokenValid(storedUser.token)) {
+      setUser(storedUser);
+      setUserValid(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user && checkTokenValid(user.token)) {
+      localStorage.setItem("user", JSON.stringify(user));
+      setUserValid(true);
+    } else {
+      localStorage.removeItem("user");
+      setUserValid(false);
+    }
+  }, [user]);
+
+  return (
+    <UserContext.Provider value={{ user, login, logout, userValid }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => {
+  const context = React.useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUserContext must be used within a UserProvider");
+  }
+  return context;
+};
