2079b0c5195a65f252f8d71f577e6bcc5ef8d75f
berkaysson <berkaysonel85@gmail.com>
2023-12-23 22:09:17 +0300

    Create navigation template

--- harmonii.client/src/layouts/AppLayout.jsx
+++ harmonii.client/src/layouts/AppLayout.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import RouterComponent from '../routers/Router';
 import { UserContextProvider } from '../services/hooks/useUser';
+import Navigation from '../components/Navigation/Navigation';
 
 const AppLayout = () => {
   return (
     <div>
       <UserContextProvider>
         <BrowserRouter>
+        <Navigation />
         {//ScrollToTop component
         // navigation, footer in browser router
         }
