3ad515a5d5f3134012f0b1fd90225acd13929aaf
berkaysson <berkaysonel85@gmail.com>
2023-12-24 14:21:41 +0300

    Implement instance of axios from api.js

--- harmonii.client/src/components/Auth/LogoutButton.jsx
+++ harmonii.client/src/components/Auth/LogoutButton.jsx
@@ -1,26 +1,14 @@
-import axios from "axios";
-import instance from "../../services/api/api";
 import { useUserContext } from "../../services/hooks/useUser";
-
-let config = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: 'https://localhost:7291/api/auth/logout',
-  headers: { 
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYmVya2F5c29uZWwiLCJqdGkiOiI1ZjJjNjQ2NC0zMTVkLTRjMDEtOWJjOS01YjY4YmQxNjZmZDYiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOlsiU3RhbmRhcmQiLCJNb2RlcmF0b3IiLCJBZG1pbiJdLCJleHAiOjE3MDM2MDAwNjksImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjcyOTEiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo1MTczIn0.l2-BT52dXA2oOL6tQaixPu6JcfhbPhhqdHWGFFpU55s'
-  }
-};
+import instance from "../../services/api/api";
 
 const LogoutButton = () => {
   const { logout } = useUserContext();
 
   const handleLogout = async () => {
-    console.log(instance.defaults);
     try {
-      const response = await axios.request(config);
-      console.log(instance.defaults);
+      const response = await instance.post("/auth/logout");
       logout();
-      console.log("logout successful", response);
+      console.log("Logout successful", response);
     } catch (error) {
       console.error(error);
     }
