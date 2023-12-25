fd418656bfd6113e7e83aa87d60e0e4c4b17b584
berkaysson <berkaysonel85@gmail.com>
2023-12-25 18:36:35 +0300

    Implement yup schema and formik to create form validations

--- harmonii.client/src/components/Auth/RegisterForm.jsx
+++ harmonii.client/src/components/Auth/RegisterForm.jsx
@@ -1,65 +1,85 @@
-import { useState } from "react";
 import { registerApi } from "../../api/registerApi";
 import { useNavigate } from "react-router";
 import { displayResponse } from "../../services/displayResponse";
+import { useFormik } from "formik";
+import { registerSchema } from "../../services/auth/schema.yup";
+import { StyledForm } from "../../styles/components/forms/Form.styles";
 
 const RegisterForm = () => {
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+  const handleFormSubmit = async (values) => {
     const registerData = {
-      userName,
-      email,
-      password,
+      userName: values.userName,
+      email: values.email,
+      password: values.password,
     };
 
     const response = await registerApi(registerData);
-    if(response.name === "AxiosError"){
+    if (response.name === "AxiosError") {
       console.log(response);
-    }
-    else{
-      if(response.data.status === "Success"){
+    } else {
+      if (response.data.status === "Success") {
         displayResponse(response);
       }
       navigate("/login");
     }
   };
 
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      userName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: registerSchema,
+    onSubmit: handleFormSubmit,
+  });
+
   return (
-    <form onSubmit={handleFormSubmit}>
-      <div>
-        <label htmlFor="userName">User Name:</label>
-        <input
-          type="text"
-          id="userName"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">Register</button>
-    </form>
+    <StyledForm onSubmit={handleSubmit} autoComplete="off">
+      <label htmlFor="userName">User Name:</label>
+      <input
+        type="text"
+        id="userName"
+        value={values.userName}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.userName && touched.userName ? "input-error" : ""}
+      />
+      {errors.userName && touched.userName && <span>{errors.userName}</span>}
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.email && touched.email ? "input-error" : ""}
+      />
+      {errors.email && touched.email && <span>{errors.email}</span>}
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className={errors.password && touched.password ? "input-error" : ""}
+      />
+      {errors.password && touched.password && <span>{errors.password}</span>}
+      <button disabled={isSubmitting} type="submit">
+        Register
+      </button>
+    </StyledForm>
   );
 };
 
