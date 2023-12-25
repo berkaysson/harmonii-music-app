3093ac283eb65c6558bcb95dbb4763328132212f
berkaysson <berkaysonel85@gmail.com>
2023-12-25 14:38:47 +0300

    Create template footer component

--- harmonii.client/src/layouts/AppLayout.jsx
+++ harmonii.client/src/layouts/AppLayout.jsx
@@ -1,12 +1,14 @@
 import RouterComponent from '../routers/Router';
 import Navigation from '../components/Navigation/Navigation';
 import StyledAppLayout from '../styles/components/layouts/AppLayout.styles';
+import Footer from '../components/Footer/Footer';
 
 const AppLayout = () => {
   return (
     <StyledAppLayout>
       <Navigation />
       <RouterComponent />
+      <Footer />
     </StyledAppLayout>
   );
 };
