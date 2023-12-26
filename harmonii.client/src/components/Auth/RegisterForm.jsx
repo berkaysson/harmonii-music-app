4415ca03ef96f84f3b3695d1589456157f9e5909
berkaysson <berkaysonel85@gmail.com>
2023-12-26 14:09:23 +0300

    Implement FormikForm

--- harmonii.client/src/components/Auth/RegisterForm.jsx
+++ harmonii.client/src/components/Auth/RegisterForm.jsx
@@ -1,9 +1,8 @@
 import { registerApi } from "../../api/registerApi";
 import { useNavigate } from "react-router";
 import { displayResponse } from "../../services/displayResponse";
-import { useFormik } from "formik";
 import { registerSchema } from "../../services/auth/schema.yup";
-import { StyledForm } from "../../styles/components/forms/Form.styles";
+import FormikForm from "../Shared/FormikForm";
 
 const RegisterForm = () => {
   const navigate = useNavigate();
@@ -26,60 +25,24 @@ const RegisterForm = () => {
     }
   };
 
-  const {
-    values,
-    errors,
-    touched,
-    isSubmitting,
-    handleBlur,
-    handleChange,
-    handleSubmit,
-  } = useFormik({
-    initialValues: {
-      userName: "",
-      email: "",
-      password: "",
-    },
-    validationSchema: registerSchema,
-    onSubmit: handleFormSubmit,
-  });
+  const fields = [
+    { id: 'userName', label: 'User Name', type: 'text' },
+    { id: 'email', label: 'Email', type: 'email' },
+    { id: 'password', label: 'Password', type: 'password' },
+  ];
 
   return (
-    <StyledForm onSubmit={handleSubmit} autoComplete="off">
-      <label htmlFor="userName">User Name:</label>
-      <input
-        type="text"
-        id="userName"
-        value={values.userName}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        className={errors.userName && touched.userName ? "input-error" : ""}
-      />
-      {errors.userName && touched.userName && <span>{errors.userName}</span>}
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        value={values.email}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        className={errors.email && touched.email ? "input-error" : ""}
-      />
-      {errors.email && touched.email && <span>{errors.email}</span>}
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        value={values.password}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        className={errors.password && touched.password ? "input-error" : ""}
-      />
-      {errors.password && touched.password && <span>{errors.password}</span>}
-      <button disabled={isSubmitting} type="submit">
-        Register
-      </button>
-    </StyledForm>
+    <FormikForm
+      initialValues={{
+        userName: "",
+        email: "",
+        password: "",
+      }}
+      validationSchema={registerSchema}
+      onSubmit={handleFormSubmit}
+      fields={fields}
+      buttonText="Register"
+    />
   );
 };
 
