8081d548a93662457af7a5cd68feef101dee1929
berkaysson <berkaysonel85@gmail.com>
2023-12-27 20:16:06 +0300

    Add redirection to home page from login and register page if user login

--- harmonii.client/src/pages/Login.jsx
+++ harmonii.client/src/pages/Login.jsx
@@ -1,6 +1,19 @@
+import { useNavigate } from 'react-router';
 import LoginForm from '../components/Auth/LoginForm';
+import { useUserContext } from '../services/hooks/useUser';
+import { useEffect } from 'react';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { userValid } = useUserContext();
+
+  useEffect(() => {
+    if(userValid){
+      navigate("/");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
 
   return (
     <div>
--- harmonii.client/src/pages/Register.jsx
+++ harmonii.client/src/pages/Register.jsx
@@ -1,6 +1,18 @@
+import { useNavigate } from "react-router";
 import RegisterForm from "../components/Auth/RegisterForm";
+import { useUserContext } from "../services/hooks/useUser";
+import { useEffect } from "react";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const { userValid } = useUserContext();
+
+  useEffect(() => {
+    if(userValid){
+      navigate("/");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
